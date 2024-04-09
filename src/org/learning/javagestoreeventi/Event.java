f81c9fab583d0188bcea2702609bde9a1ea9156d
umbertofaenza <umberto.faenza@gmail.com>
2024-04-09 09:41:09 +0200

    Event: fields and constructor

---
+++ src/org/learning/javagestoreeventi/Event.java
@@ -0,0 +1,27 @@
+package org.learning.javagestoreeventi;
+
+import java.time.LocalDate;
+
+public class Event {
+    private String title;
+    private LocalDate date;
+    private int venueCapacity;
+    private int bookings;
+
+    public Event(String title, LocalDate date, int venueCapacity) throws IllegalArgumentException {
+        validateDate(date);
+        validateCapacity(venueCapacity);
+        this.title = title;
+        this.date = date;
+        this.venueCapacity = venueCapacity;
+        bookings = 0;
+    }
+
+    private void validateDate(LocalDate date) {
+        if(date.isAfter(LocalDate.now())) throw new IllegalArgumentException();
+    }
+
+    private void validateCapacity(int capacity) {
+        if(capacity <= 0) throw new IllegalArgumentException();
+    }
+}
